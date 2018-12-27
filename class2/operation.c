1e9985f98eee7817077bbbe92051e636356a14e5
eric-yoo <ajoije729@gmail.com>
2018-12-27 19:48:40 +0900

    class2, operation practice

---
+++ class2/operation.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int a1, a2, a3, a4, a5, a6, a7;
+	scanf("%d", &a1);
+	scanf("%d", &a2);
+  scanf("%d", &a3);
+  scanf("%d", &a4);
+  scanf("%d", &a5);
+  scanf("%d", &a6);
+  scanf("%d", &a7);
+	// int result=a1+a2;
+	// result=result+(a1+a2+a3)/3;
+	// result=result*a4;
+	// int x=result-(a5/a6);
+	// x=x%a7;
+	// printf("%d\n",a7);
+  int sum;
+  sum = a1+ a7;
+  printf("%d\n",sum);
+}
